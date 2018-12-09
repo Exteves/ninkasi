54c195e2b63b2e044d17929266d4cef869dddbea
Renan <mirandarenan.e@gmail.com>
2018-12-09 18:20:28 -0200

    get all flavors route finished

--- src/Handler/Flavor.hs
+++ src/Handler/Flavor.hs
@@ -9,14 +9,20 @@ import Import
 import Handler.Funcs as F
 
 optionsFlavorR :: Handler ()
-optionsFlavorR = F.anyOriginIn [ F.OPTIONS, F.POST ]
+optionsFlavorR = F.anyOriginIn [ F.OPTIONS, F.POST, F.GET ]
 
 -- POST --
 
 postFlavorR :: Handler Value
 postFlavorR = do
+    F.anyOriginIn [ F.OPTIONS, F.POST ]
     flavor    <-  requireJsonBody :: Handler Flavor
     flavorId  <-  runDB $ insert flavor
     sendStatusJSON created201 $ object [ "resp" .= flavorId ]
 
+getFlavorR :: Handler Value
+getFlavorR = do
+    F.anyOriginIn [ F.OPTIONS, F.GET ]
+    flavors   <-  runDB $ selectList [] [ Asc FlavorName ]
+    sendStatusJSON ok200 $ object [ "resp" .= flavors ]
 
