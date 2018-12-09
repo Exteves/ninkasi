515e1f29bd5de526afe1b687af8fd62bfb38147b
Renan <mirandarenan.e@gmail.com>
2018-12-09 17:47:53 -0200

    get all packing types route

--- src/Handler/Packing.hs
+++ src/Handler/Packing.hs
@@ -9,14 +9,19 @@ import Import
 import Handler.Funcs as F
 
 optionsPackingR :: Handler ()
-optionsPackingR = F.anyOriginIn [ F.OPTIONS, F.POST ]
+optionsPackingR = F.anyOriginIn [ F.OPTIONS, F.POST, F.GET ]
 
 -- POST --
 
 postPackingR :: Handler Value
 postPackingR = do
+    F.anyOriginIn [ F.OPTIONS, F.POST ]
     packing    <-  requireJsonBody :: Handler Packing
     packingId  <-  runDB $ insert packing
     sendStatusJSON created201 $ object [ "resp" .= packingId ]
 
-
+getPackingR :: Handler Value
+getPackingR = do
+    F.anyOriginIn [ F.OPTIONS, F.GET ]
+    packtypes   <-  runDB $ selectList [] [ Asc PackingName ]
+    sendStatusJSON ok200 $ object [ "resp" .= packtypes ]
