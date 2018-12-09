046f111feb1d58686c16be89748b9995344ce891
Renan <mirandarenan.e@gmail.com>
2018-12-09 18:25:05 -0200

    get all styles route finished

--- src/Handler/Style.hs
+++ src/Handler/Style.hs
@@ -9,14 +9,21 @@ import Import
 import Handler.Funcs as F
 
 optionsStyleR :: Handler ()
-optionsStyleR = F.anyOriginIn [ F.OPTIONS, F.POST ]
+optionsStyleR = F.anyOriginIn [ F.OPTIONS, F.POST, F.GET ]
 
 -- POST --
 
 postStyleR :: Handler Value
 postStyleR = do
+    F.anyOriginIn [ F.OPTIONS, F.POST ]
     style    <-  requireJsonBody :: Handler Style
     styleId  <-  runDB $ insert style
     sendStatusJSON created201 $ object [ "resp" .= styleId ]
+
+getStyleR :: Handler Value
+getStyleR = do
+    F.anyOriginIn [ F.OPTIONS, F.GET ]
+    styles   <-  runDB $ selectList [] [ Asc StyleName ]
+    sendStatusJSON ok200 $ object [ "resp" .= styles ]
 
 
