8e10e17e2927d56e83b9f17dccc8f22ff019b553
Renan <mirandarenan.e@gmail.com>
2018-12-09 17:43:32 -0200

    Get All countries route

--- src/Handler/Country.hs
+++ src/Handler/Country.hs
@@ -9,14 +9,21 @@ import Import
 import Handler.Funcs as F
 
 optionsCountryR :: Handler ()
-optionsCountryR = F.anyOriginIn [ F.OPTIONS, F.POST ]
+optionsCountryR = F.anyOriginIn [ F.OPTIONS, F.POST, F.GET ]
 
 -- POST --
 
 postCountryR :: Handler Value
 postCountryR = do
+    F.anyOriginIn [ F.OPTIONS, F.POST ]
     country    <-  requireJsonBody :: Handler Country
     countryId  <-  runDB $ insert country
     sendStatusJSON created201 $ object [ "resp" .= countryId ]
+
+getCountryR :: Handler Value
+getCountryR = do
+    F.anyOriginIn [ F.OPTIONS, F.GET ]
+    countries   <-  runDB $ selectList [] [ Asc CountryName ]
+    sendStatusJSON ok200 $ object [ "resp" .= countries ]
 
 
