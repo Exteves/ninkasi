0d81c12318672b952ebe497d1bb057e220ef46f7
Renan <mirandarenan.e@gmail.com>
2018-12-09 18:17:55 -0200

    get all colors route finished

--- src/Handler/Color.hs
+++ src/Handler/Color.hs
@@ -9,14 +9,19 @@ import Import
 import Handler.Funcs as F
 
 optionsColorR :: Handler ()
-optionsColorR = F.anyOriginIn [ F.OPTIONS, F.POST ]
+optionsColorR = F.anyOriginIn [ F.OPTIONS, F.POST, F.GET ]
 
 -- POST --
 
 postColorR :: Handler Value
 postColorR = do
+    F.anyOriginIn [ F.OPTIONS, F.POST ]
     color    <-  requireJsonBody :: Handler Color
     colorId  <-  runDB $ insert color
     sendStatusJSON created201 $ object [ "resp" .= colorId ]
 
-
+getColorR :: Handler Value
+getColorR = do
+    F.anyOriginIn [ F.OPTIONS, F.GET ]
+    colors   <-  runDB $ selectList [] [ Asc ColorName ]
+    sendStatusJSON ok200 $ object [ "resp" .= colors ]
