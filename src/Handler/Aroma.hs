24510c1d6c2cc4609ca72276227529ecd04a70cb
Renan <mirandarenan.e@gmail.com>
2018-12-09 17:59:14 -0200

    get all smells route finished

--- src/Handler/Aroma.hs
+++ src/Handler/Aroma.hs
@@ -11,14 +11,19 @@ import Handler.Funcs as F
 --OPTIONS--
 
 optionsAromaR :: Handler ()
-optionsAromaR = F.anyOriginIn [ F.OPTIONS, F.POST ]
+optionsAromaR = F.anyOriginIn [ F.OPTIONS, F.POST, F.GET ]
 
 -- POST --
 
 postAromaR :: Handler Value
 postAromaR = do
+    F.anyOriginIn [ F.OPTIONS, F.POST ]
     aroma    <-  requireJsonBody :: Handler Aroma
     aromaId  <-  runDB $ insert aroma
     sendStatusJSON created201 $ object [ "resp" .= aromaId ]
 
-
+getAromaR :: Handler Value
+getAromaR = do
+    F.anyOriginIn [ F.OPTIONS, F.GET ]
+    smells   <-  runDB $ selectList [] [ Asc AromaName ]
+    sendStatusJSON ok200 $ object [ "resp" .= smells ]
