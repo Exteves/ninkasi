e9a7724fe1f8db510deafd42abf68bce5929b1fe
dgbarbosa <dgbarbosa96@gmail.com>
2018-12-10 22:20:10 -0200

    country by id route finished

--- src/Handler/Country.hs
+++ src/Handler/Country.hs
@@ -11,6 +11,9 @@ import Handler.Funcs as F
 optionsCountryR :: Handler ()
 optionsCountryR = F.anyOriginIn [ F.OPTIONS, F.POST, F.GET ]
 
+optionsCountryByIdR :: CountryId -> Handler ()
+optionsCountryByIdR _ = F.anyOriginIn [ F.OPTIONS, F.GET ]
+
 -- POST --
 
 postCountryR :: Handler Value
@@ -26,4 +29,8 @@ getCountryR = do
     countries   <-  runDB $ selectList [] [ Asc CountryName ]
     sendStatusJSON ok200 $ object [ "resp" .= countries ]
 
-
+getCountryByIdR :: CountryId -> Handler Value
+getCountryByIdR countryId = do
+    F.anyOriginIn [ F.OPTIONS, F.GET ]
+    country   <-  runDB $ get404 countryId
+    sendStatusJSON ok200 $ object [ "resp" .= country ]
